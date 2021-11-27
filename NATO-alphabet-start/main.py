020ad458c6ef379bd46e3378731378605e299d89
luzikato <anasnurf99@gmail.com>
2021-11-27 11:53:56 +0700

    Added KeyError exception catch for number input.

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -26,7 +26,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 pass_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word_input = input("Enter a word: ")
-coded = [pass_dict[letter.upper()] for letter in word_input]
-print(coded)
+def coder():
+    try:
+        word_input = input("Enter a word: ")
+        coded = [pass_dict[letter.upper()] for letter in word_input]
+    except KeyError:
+        print("Sorry, only letter in the alphabet please.")
+        coder()
+    else:
+        print(coded)
+coder()
+
 
